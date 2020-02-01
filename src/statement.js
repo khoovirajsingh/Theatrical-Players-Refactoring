f411641743714a3a67936fd721026284a774e165
khoovis <khoovirajsingh.seewoonauth@mkodo.com>
2020-02-01 10:19:14 +0000

    Changed function declaration to use only 1 parameter

--- src/statement.js
+++ src/statement.js
@@ -8,7 +8,7 @@ function statement (invoice, plays) {
             minimumFractionDigits: 2 }).format;
 
     for (let perf of invoice.performances) {
-        let thisAmount = amountFor(playFor(perf), perf);
+        let thisAmount = amountFor(perf);
         // add volume credits
         volumeCredits += Math.max(perf.audience - 30, 0);
         // add extra credit for every ten comedy attendees
@@ -25,9 +25,9 @@ function statement (invoice, plays) {
         return plays[aPerformance.playID];
     }
 
-    function amountFor (play, aPerformance) {
+    function amountFor (aPerformance) {
         let result = 0;
-        switch (play.type) {
+        switch (playFor(aPerformance).type) {
             case "tragedy":
                 result = 40000;
                 if (aPerformance.audience > 30) {
@@ -42,7 +42,7 @@ function statement (invoice, plays) {
                 result += 300 * aPerformance.audience;
                 break;
             default:
-                throw new Error(`unknown type: ${play.type}`);
+                throw new Error(`unknown type: ${playFor(aPerformance).type}`);
         }
         return result;
     }
