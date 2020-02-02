f8c3a51754a04d4cc05d0a5bec873a4f27d41421
khoovis <khoovirajsingh.seewoonauth@mkodo.com>
2020-02-02 12:22:59 +0000

    Applied Replace Conditional with Polymorphism

--- src/createStatementData.js
+++ src/createStatementData.js
@@ -45,9 +45,24 @@ function createPerformanceCalculator(aPerformance, aPlay) {
 }
 
 class TragedyCalculator extends PerformanceCalculator {
+    get amount () {
+        let result =  40000;
+        if (this.performance.audience > 30) {
+            result += 1000 * (this.performance.audience - 30);
+        }
+        return result;
+    }
 }
 
 class ComedyCalculator extends PerformanceCalculator {
+    get amount () {
+        let result =  30000;
+        if (this.performance.audience > 20) {
+            result += 10000 + 500 * (this.performance.audience - 20);
+        }
+        result += 300 * this.performance.audience;
+        return result;
+    }
 }
 
 
