1893abafb34e95095135002ff24d5554ddc81a5f
khoovis <khoovirajsingh.seewoonauth@mkodo.com>
2020-02-02 12:20:46 +0000

    Applied Replace Type Code with Subclasses

--- src/createStatementData.js
+++ src/createStatementData.js
@@ -36,7 +36,20 @@ class PerformanceCalculator {
 
 function createPerformanceCalculator(aPerformance, aPlay) {
     return new PerformanceCalculator(aPerformance, aPlay);
+    switch(aPlay.type) {
+    case "tragedy": return new TragedyCalculator(aPerformance, aPlay);
+    case "comedy": return new ComedyCalculator(aPerformance, aPlay);
+    default:
+        throw new Error('unknown type: ${aPlay.type}');
+    }
 }
+
+class TragedyCalculator extends PerformanceCalculator {
+}
+
+class ComedyCalculator extends PerformanceCalculator {
+}
+
 
 function createStatementData (invoice, plays) {
     const result = {};
